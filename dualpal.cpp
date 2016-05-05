ea11680b4e1dfc8faf9fe310cc35a64f1e566828
ericjam3 <ericjam@umich.edu>
2016-05-05 18:04:12 -0400

    dualpal

---
+++ dualpal.cpp
@@ -0,0 +1,25 @@
+/*
+ID: ericm101
+LANG: C++
+TASK: palsquare
+*/
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	ifstream in("dualpal.in");
+	int N, S;
+	in >> N >> S;
+
+	in.close();
+	return 0;
+}
