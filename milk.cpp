6bf0a8862011b11ec4bf413b7376159125c6c03e
Eric James Mier <ericjam@caen-vnc16.engin.umich.edu>
2016-05-05 19:11:05 -0400

    almost done

--- milk.cpp
+++ milk.cpp
@@ -19,14 +19,23 @@ struct farmer_info{
 	int amount;
 };
 
+bool comp(const farmer_info i, const farmer_info j){
+	return i.cost < j.cost;
+}
+
 int main(){
 	ifstream in("milk.in");
 	ofstream out("milk.out");
-	int milk_needed, num_farmers;
+	int milk_needed, num_farmers, total_milk = 0, ind = 0;
 	in >> milk_needed >> num_farmers;
 	vector<farmer_info> data(num_farmers);
 	for (int i = 0; i < num_farmers; ++i){
 		in >> data[i].cost >> data[i].amount;
+	}
+	sort(data.begin(), data.end(), comp);
+	while (total_milk < milk_needed){
+		if ()
+		ind++;
 	}
 	in.close();
 	out.close();
