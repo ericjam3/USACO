cadcfec05cb9575690d22c5df7742eb307a2f042
ericjam3 <ericjam@umich.edu>
2016-05-05 16:27:22 -0400

    need to comment still

--- palsquare.cpp
+++ palsquare.cpp
@@ -5,10 +5,13 @@ TASK: palsquare
 */
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 #include <cmath>
 #include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -23,7 +26,7 @@ int main(){
 			break;
 		case 3:
 			start = 10;
-			begin = 4;
+			begin = 5;
 			break;
 		case 4: 
 			start = 8;
@@ -47,20 +50,62 @@ int main(){
 			break;	
 	} // switch
 
-	int num;
 	
+
 	ofstream out("palsquare.out");
 	for (int i = 1; i <= 300; ++i){
 		bool first = false, ispal = true;
-		vector<int> palind;
-		num = pow(i, 2);
+		vector<string> palind;
+		int num = pow(i, 2), temp;
 		for (int k = start; k >= 0; --k){
 			if (pow(base, k) <= num){
-				palind.push_back(num / pow(base, k));
-				num -= palind.back() * pow(base, k);
+				temp = num / pow(base, k);
+				if (temp >= 10){
+					switch (temp){
+						case 10:
+							palind.push_back("A");
+							break;
+						case 11:
+							palind.push_back("B");
+							break;
+						case 12:
+							palind.push_back("C");
+							break;
+						case 13:
+							palind.push_back("D");
+							break;
+						case 14:
+							palind.push_back("E");
+							break;
+						case 15:
+							palind.push_back("F");
+							break;
+						case 16:
+							palind.push_back("G");
+							break;
+						case 17:
+							palind.push_back("H");
+							break;
+						case 18:
+							palind.push_back("I");
+							break;
+						default:
+							palind.push_back("J");
+							break;
+					}
+				}else{
+					stringstream ss;
+					ss << temp;
+					string tempstring = ss.str();
+					palind.push_back(tempstring);
+				}
+				num -= temp * pow(base, k);
 				first = true;
 			}else if (first){
-				palind.push_back(0);
+				stringstream ss;
+				ss << 0;
+				string tempstring = ss.str();
+				palind.push_back(tempstring);
 			}
 		}
 		for (unsigned d = 0; d <= palind.size() / 2; ++d){
@@ -71,16 +116,58 @@ int main(){
 		}
 
 		if (ispal){
-			int num2 = i;
+			int num2 = i, temp2;
 			bool f = false;
-			vector<int> pal;
+			vector<string> pal;
 			for (int k = begin; k >= 0; --k){
 				if (pow(base, k) <= num2){
-					pal.push_back(num2 / pow(base, k));
-					num2 -= pal.back() * pow(base, k);
+					temp2 = num2 / pow(base, k);
+					if (temp2 >= 10){
+						switch (temp2){
+							case 10:
+								pal.push_back("A");
+								break;
+							case 11:
+								pal.push_back("B");
+								break;
+							case 12:
+								pal.push_back("C");
+								break;
+							case 13:
+								pal.push_back("D");
+								break;
+							case 14:
+								pal.push_back("E");
+								break;
+							case 15:
+								pal.push_back("F");
+								break;
+							case 16:
+								pal.push_back("G");
+								break;
+							case 17:
+								pal.push_back("H");
+								break;
+							case 18:
+								pal.push_back("I");
+								break;
+							default:
+								pal.push_back("J");
+								break;
+						}
+					}else{
+						stringstream ss;
+						ss << temp2;
+						string tempstring = ss.str();
+						pal.push_back(tempstring);
+					}
+					num2 -= temp2 * pow(base, k);
 					f = true;
 				}else if (f){
-					pal.push_back(0);
+					stringstream ss;
+					ss << 0;
+					string tempstring = ss.str();
+					pal.push_back(tempstring);
 				}
 			}
 			
