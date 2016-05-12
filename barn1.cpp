6441fb20c27a56437165c0050e4e73db52c677ac
Eric James Mier <ericjam@caen-vnc20.engin.umich.edu>
2016-05-12 09:36:47 -0400

    first compile

---
+++ barn1.cpp
@@ -0,0 +1,50 @@
+/*
+ID: ericm101
+LANG: C++
+TASK: barn1
+*/
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(){
+	ifstream in("barn1.in");
+	ofstream out("barn1.out");
+	int boards, stalls, cows, current, first, last, next;
+	in >> boards >> stalls >> cows >> current;
+	first = current;
+	vector<int> gaps;
+
+	for (int i = 1; i < cows; ++i){
+		in >> next;
+		last = next;
+		gaps.push_back(next - current - 1);
+		current = next;
+	}
+
+	sort(gaps.begin(), gaps.end());
+
+	int total = stalls - last - first - 1;
+	unsigned min = boards - 1;
+	if (gaps.size() < min){
+		min = gaps.size();
+	}
+
+	for (unsigned i = 0; i < min; ++i){
+		total -= gaps[gaps.size() - i - 1];
+	}
+	cout << total << endl;
+
+
+	in.close();
+	out.close();
+	return 0;
+}
