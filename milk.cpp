401e43a0420c3808ee091ced9ecf72398af65bce
ericjam3 <ericjam@umich.edu>
2016-05-29 15:31:49 -0400

    added some comments

--- milk.cpp
+++ milk.cpp
@@ -4,6 +4,20 @@ LANG: C++
 TASK: milk
 */
 
+/*
+The purpose of this program is to find the way for a company to
+purchase a specified amount of milk at the cheapest cost. The company
+has a list of farmers that it will buy from and for each of those farmers,
+we have information on how much milk they have and how much it costs per
+unit. The input is formatted as follows: The first line contains two integers,
+N and M. N is the amount of milk needed and M is the number of farmers to buy
+it from. The next M lines contain two integers each, the first is the cost per
+unit and the second is the amount of milk available. With this information, 
+the program will determine the cheapest way to get the needed amount of milk.
+Note that you can buy a portion of a farmer's available milk, there is no need
+to buy them out! Outputted is the minimum cost for the milk
+*/
+
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -14,14 +28,17 @@ TASK: milk
 
 using namespace std;
 
+// organizes information for each farmer
 struct farmer_info{
 	int cost;
 	int amount;
 };
 
+// Used for sorting buying options in terms of cost per unit
 bool comp(const farmer_info i, const farmer_info j){
 	return i.cost < j.cost;
 }
+
 
 int main(){
 	ifstream in("milk.in");
@@ -30,10 +47,14 @@ int main(){
 	unsigned ind = 0;
 	in >> milk_needed >> num_farmers;
 	vector<farmer_info> data(num_farmers);
+
+	// Reading information on different buying options and saving
+	// it in a vector of structs
 	for (int i = 0; i < num_farmers; ++i){
 		in >> data[i].cost >> data[i].amount;
 	}
 	
+	// sorting the vector of buying options in terms of unit cost
 	sort(data.begin(), data.end(), comp);
 	
 	while (total_milk < milk_needed){
