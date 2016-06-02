fe217e6e4adc1d73364771199ae052e09501da53
ericjam3 <ericjam@umich.edu>
2016-06-01 20:20:00 -0400

    added some comments

--- crypt1.cpp
+++ crypt1.cpp
@@ -4,6 +4,27 @@
 
 using namespace std;
 
+/*
+This program reads from a text file named "crypt1.in". It first reads a single integer
+which represents the number of digits that will appear on the line below, separated
+by spaces. Those digits, ranging between 1 and 9 inclusive, will then be read in. The
+purpose of the program is to find all the solutions of a cryptarithm involving the
+multiplication of the given digits and must result in the use of only the given digits.
+To make it more clear, here is an example:
+Given:
+5
+2 3 4 6 8
+
+The only solution that will work is:
+ 222
+ *22
+____
+ 444
+444
+____
+4884
+ */
+
 
 class Crypt1 {
 public:
