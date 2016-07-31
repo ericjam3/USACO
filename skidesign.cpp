abc6205ec0d95dd166b754d3bb7118eaa5ea0724
ericjam3 <ericjam@umich.edu>
2016-07-30 23:38:14 -0400

    added comments to ski design

--- skidesign.cpp
+++ skidesign.cpp
@@ -12,10 +12,24 @@ TASK: skidesign
 
 using namespace std;
 
+/*
+This program correctly solves the ski design USACO problem. First, it reads from
+an input file formatted such that the first number, n, is an integer from 1 to 1000.
+After that, there are n more numbers in the file, each between 0 and 100 inclusive.
+Each number represents the height of a ski hill. The owner of the ski resort wants
+to make it so that his tallest and shortest hills are within 17 feet of each other in
+height so that he can bypass a certain tax. He must shorten and heighten hills accordingly,
+and each hill costs x^2 where x is change in height. This program goes through every range
+of 17 from 0 to 100 and finds the range which will result in the least cost. It uses branch
+and bound to speed it up for larger test cases.
+*/
+
 class Ski {
 public:
+	// constructor
 	Ski();
 
+	// driver function to run program
 	void run_this_sucker();
 
 private:
@@ -43,6 +57,9 @@ Ski::Ski() {
 	sort(hills.begin(), hills.end());
 	in.close();
 	min_cost = 0;
+
+	// calculate a reasonably low min_cost to start with
+	// to help bounding the amount of work done by the program
 	for (int i = 0; i < num_hills; ++i) {
 		if (hills[i] < 42) {
 			min_cost += pow(42 - hills[i], 2);
@@ -51,7 +68,6 @@ Ski::Ski() {
 			min_cost += pow(hills[i] - 59, 2);
 		}
 	}
-	
 }
 
 void Ski::run_this_sucker() {
