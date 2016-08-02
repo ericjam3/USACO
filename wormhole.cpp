d9f65c95137253d98c7fcd6bca8c2f2ed132d310
ericjam3 <ericjam@umich.edu>
2016-08-02 15:36:39 -0400

    keeping repo up to date

--- wormhole.cpp
+++ wormhole.cpp
@@ -7,13 +7,21 @@ TASK: wormhole
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-struct coords {
+struct Coords {
 	int x;
 	int y;
 };
+
+bool comparator(Coords a, Coords b) {
+	if (a.y < b.y || (a.y == b.y && a.x < b.x)) {
+		return true;
+	}
+	return false;
+}
 
 class Worm {
 public:
@@ -22,14 +30,22 @@ public:
 private:
 	int num_holes;
 	int count;
-	vector<coords> wormholes;
+	int current_ind;
+	vector<Coords> wormholes;
+	vector<bool> used;
+	vector<int> pairings;
 
 	void set_up();
+
+	void pair_up(int num);
+
+	void traverse_paths();
 };
 
 int main() {
 	Worm obj;
 	obj.run();
+
 	return 0;
 }
 
@@ -38,23 +54,83 @@ void Worm::set_up() {
 	ifstream in("wormhole.in");
 	in >> num_holes;
 	wormholes.resize(num_holes);
+	used.resize(num_holes);
+	pairings.resize(num_holes);
 	for (int i = 0; i < num_holes; ++i) {
 		in >> wormholes[i].x >> wormholes[i].y;
+		used[i] = false;
 	}
 	in.close();
+	sort(wormholes.begin(), wormholes.end(), comparator);
 }
 
 void Worm::run() {
 	set_up();
-	for (int i = 0; i < num_holes; ++i) {
-		for (int j = 0; j < num_holes; ++j) {
-			if (wormholes[i].y == wormholes[j].y && wormholes[i].x < wormholes[j].x) {
-				++count;
+	used[0] = true;
+	current_ind = 0;
+	pair_up(0);
+	ofstream out("wormhole.out");
+	out << count << endl;
+	out.close();
+}
+
+void Worm::pair_up(int num) {
+	if (num < (num_holes / 2) - 1) {
+		int old_ind = current_ind;
+		for (int i = old_ind + 1; i < num_holes; ++i) {
+			if (!used[i]) {
+				pairings[i] = old_ind;
+				pairings[old_ind] = i;
+				used[i] = true;
+				for (int j = old_ind + 1; j < num_holes; ++j) {
+					if (!used[j]) {
+						current_ind = j;
+						used[current_ind] = true;
+						break;
+					}
+				}
+				pair_up(num + 1);
+				used[i] = false;
+				used[current_ind] = false;
+			}
+		}
+	}
+	else {
+		for (int i = current_ind + 1; i < num_holes; ++i) {
+			if (!used[i]) {
+				pairings[i] = current_ind;
+				pairings[current_ind] = i;
+				used[i] = true;
+				traverse_paths();
+				used[i] = false;
 				break;
 			}
 		}
 	}
-	ofstream out("wormhole.out");
-	out << count << "\n";
-	out.close();
+}
+
+void Worm::traverse_paths() {
+	for (int i = 0; i < num_holes; ++i) {
+		int current = i;
+		vector<bool> visited(num_holes, false);
+		visited[i] = true;
+		while (1) {
+			current = pairings[current];
+			if (visited[current]) {
+				count++;
+				return;
+			}
+			visited[current] = true;
+			if ((current + 1 >= num_holes) || wormholes[current + 1].y != wormholes[current].y) {
+				break;
+			}else{
+				current = current + 1;
+				if (visited[current]) {
+					count++;
+					return;
+				}
+				visited[current] = true;
+			}
+		}
+	}
 }
